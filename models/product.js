fffc9a55bc7de526efc64d60babd9ca3f09bf32a
Maxim Zubakin <zubakin@sibext.com>
2020-02-20 18:37:40 +0700

    routes, enhancing app

--- models/product.js
+++ models/product.js
@@ -2,35 +2,47 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (callback) => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            return callback([]);
-        } else {
-            callback(JSON.parse(fileContent));
-        }
-    });
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-    }
-    save() {
-        getProductsFromFile((products) => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    static fetchAll(callback) {
-        getProductsFromFile(callback)
-    }
-}
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      cb(product)
+    });
+  }
+};
